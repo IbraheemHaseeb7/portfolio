69dab230e8607194d54cf86de636c9a05ee574e1
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-06-15 22:18:34 +0500

    (core)[bug] - replaced typed.js with react-type-animation
    - the previous library had issues with typescript so I had to change it
      with this libary which works smoothly and has more concise code.

--- components/HomeCenter.tsx
+++ components/HomeCenter.tsx
@@ -1,45 +1,36 @@
-import { useEffect, useRef } from "react";
 import { MutableRefObject } from "react";
-import Typed from "typed.js";
+import { TypeAnimation } from "react-type-animation";
 
 export default function HomeCenter({
     refer,
 }: {
     refer: MutableRefObject<any>;
 }) {
-    const name = useRef(null);
-    const title = useRef(null);
-
-    // ADDING THE ANIMATION
-    useEffect(() => {
-        // IMPLEMENTING THE ANIMATION
-        const nameTyped = new Typed(name.current, {
-            strings: [`Ibraheem Bin Haseeb`],
-            typeSpeed: 50,
-            showCursor: false,
-        });
-        const titleTyped = new Typed(title.current, {
-            strings: [
-                `^2000Full Stack Web Developer, DevOps Enthusiast <br> Data Scientist to be`,
-            ],
-            typeSpeed: 50,
-            showCursor: false,
-        });
-
-        // CLEANING UP ALL THE ANIMATIONS WHEN THE COMPONENT IS DISMOUNTED
-        return () => {
-            nameTyped.destroy();
-            titleTyped.destroy();
-        };
-    }, []);
-
     return (
         <div
             ref={refer}
             className="flex flex-col items-center justify-center w-2/3 h-1/3 m-0 p-0 gap-5 z-10"
         >
-            <h1 ref={name} className="text-4xl m-0 p-0"></h1>
-            <p ref={title} className="text-2xl text-center m-0 p-0"></p>
+            <TypeAnimation
+                sequence={["Ibraheem Bin Haseeb"]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: "2rem", display: "inline-block" }}
+                cursor={false}
+                repeat={1}
+            />
+            <TypeAnimation
+                sequence={[
+                    1000,
+                    "Full Stack Developer, DevOps and MLOps Enthusiast",
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: "1rem", display: "inline-block" }}
+                className=""
+                repeat={1}
+                cursor={false}
+            />
         </div>
     );
 }
